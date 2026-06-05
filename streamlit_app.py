b806365a188aa5a7ae3c4d87514eb23e64c51678
2610729-dot <2610729@sh.re.kr>
2026-06-05 03:32:54 +0000

    ass

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
-import streamlit as st
+height = int(input('키(cm): '))
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+if height < 100:
+    print('탑승 불가')
+elif height < 130:  # 100 이상 130 미만인 경우
+    print('보호자 동행시 탑승가능')
+elif height < 195:  # 130 이상 195 미만인 경우
+    print('탑승가능')
+else:              # 195 이상인 모든 경우
+    print('탑승불가')
